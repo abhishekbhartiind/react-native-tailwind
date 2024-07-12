948e14ea53751936cd3635156b63b1bd74c4ae4c
Abhishek Bharti <abhishek.bharti@3insys.com>
2024-07-12 19:37:09 +0530

    React Native Expo with Tailwind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,15 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-center mt-3 text-2xl font-light text-orange-300'>
+        Tab One
+      </Text>
+      <View style={styles.separator} />
+      <EditScreenInfo path='app/(tabs)/index.tsx' />
     </View>
   );
 }
